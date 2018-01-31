16f39c1b8bb91a5a6d6059c7460d676bf0dbc11f
Felix Kern <kernfel+github@gmail.com>
2018-01-31 19:14:07 +0000

    Reordered & polished Kumaravelu raster

--- KumaraveluEtAl2016/rasterplot.m
+++ KumaraveluEtAl2016/rasterplot.m
@@ -2,8 +2,8 @@ function [] = rasterplot()
 
   load('simulate_network_model')
 
-  strings = {'Ctx'; 'GPe'; 'GPi'; 'STN'; 'D1'; 'D2'; 'TH'};
-  aps = [Cor_APs, GPe_APs, GPi_APs, STN_APs, Striat_APs_dr, Striat_APs_indr, TH_APs];
+  strings = {'Ctx'; 'MSN D1'; 'MSN D2'; 'STN'; 'GPe'; 'GPi'; 'Thal'}
+  aps = [Cor_APs, Striat_APs_dr, Striat_APs_indr, STN_APs, GPe_APs, GPi_APs, TH_APs];
   len = [20, 10, 10, 10, 10, 10, 10];
   n = 7;
   k = 0;
@@ -20,10 +20,16 @@ function [] = rasterplot()
       dots = [dots; ones(length(ap.times), 1) .* i];
       nspk = nspk + length(ap.times);
     end
-    plot(ax, times, dots, 'LineStyle', 'none', 'Marker', '.');
+    plot(ax, times, dots, 'LineStyle', 'none', 'Marker', '.', 'Color', 'black');
     xlim(ax, [0,2])
     ylim(ax, [0,len(j)])
     ylabel(ax, strings{j})
+    if j == n
+      axis('ticx', 'labelx')
+    else
+      axis('ticy', 'nolabel')
+    end
+    
     disp(strings{j}); disp(nspk / 2. / len(j))
   end
 end
